1431eb37db63b5da7b07ebb95e6ad1d4227b4727
RohitNunnaguppala <151500766+RohitNunnaguppala@users.noreply.github.com>
2024-12-16 23:30:50 +0530

    Create Permutations in array.cpp

---
+++ Day7/Permutations in array.cpp
@@ -0,0 +1,12 @@
+ bool permutationExists(int arr1[], int arr2[], int n, int k) {
+        sort(arr1, arr1 + n);
+        
+        sort(arr2, arr2 + n, greater<int>());
+        for (int i = 0; i < n; i++) {
+            if (arr1[i] + arr2[i] < k) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
